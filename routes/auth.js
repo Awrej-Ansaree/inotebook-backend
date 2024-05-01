615833fbcce621bfc8cfe2d8a204758da979dc6b
Awrej <awrejansaree786@gmail.com>
2024-05-01 19:42:13 +0545

    storing data into database using mongoose model

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  obj = {
-    id: 1,
-    name: "Awrej",
-  };
-
-  res.json(obj);
+// Create a User using: POST "/api/auth". Doesn't require Auth
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const user = User(req.body);
+  user.save().then(() => {
+    console.log("User created successfully");
+  });
+  res.send(req.body);
 });
 
 module.exports = router;
